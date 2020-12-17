9abb4b94eb021b68acaa9b214484e5e0c0f40a38
Alexander Rovang <wolf_vii@yahoo.com>
2020-12-17 14:09:47 -0500

    commit tree 5905cd53533b7a58bddfedb97deac496b341a0b3

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
+require "pry"
+
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :artist_name, :name
   @@all = []
 
   def self.all
@@ -8,6 +10,59 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |song|
+      song.name == name
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    files = filename.split(/[-.]/)
+    artist = files[0].strip
+    song_name = files[1].strip
+
+    song = Song.new
+    song.artist_name = artist
+    song.name = song_name
+    binding.pry
+  end
+
+  def self.create_from_filename
+    
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
